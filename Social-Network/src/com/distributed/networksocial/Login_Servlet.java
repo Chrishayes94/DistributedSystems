f9e03841a5c9c67d970343ad7fdd17178677d293
Chris <Chris@DESKTOP-VH8690U>
2016-11-06 20:03:16 +0000

    Post update template code.
    Created a template for posting a new status update, I will discuss what
    it is doing if required. Currently stores it locally in Servlet so we
    can test it easier for debugging.

--- Social-Network/src/com/distributed/networksocial/Login_Servlet.java
+++ Social-Network/src/com/distributed/networksocial/Login_Servlet.java
@@ -1,13 +1,22 @@
 package com.distributed.networksocial;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import org.datanucleus.util.Base64;
+
 import com.distributed.networksocial.handlers.LoginDao;
+import com.distributed.networksocial.handlers.NewsFeedDAO;
 
 /**
  * Basis for our application, this entry point servlet will represent the main screen (Login, Register).
@@ -20,26 +29,66 @@ import com.distributed.networksocial.handlers.LoginDao;
 @SuppressWarnings("serial")
 public class Login_Servlet extends HttpServlet {
 	
+	private static String[] expandArray(String[] destination, String[] source) {
+		if (destination.length <= destination.length + source.length) {
+			String[] expandedArray = new String[destination.length + source.length + 2];
+			
+			int count = 0;
+			for (String s : source)
+				expandedArray[count++] = s;
+			for (String s : destination)
+				expandedArray[count++] = s;
+			
+			return expandedArray;
+		}
+		
+		return destination;
+	}
+
+	String[] updates = null;
+	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		resp.setContentType("text/plain");
-		PrintWriter out = resp.getWriter();
 		
-		final String name = req.getParameter("username");
+		final String name = (String)req.getParameter("usename");
 		final String password = req.getParameter("password");
 		
 		// Determine request type
-		final String value = req.getParameter("value");
-		System.out.print(value);
+		final String value = req.getParameter("post");
 		
-		if (LoginDao.validate(name, password)) {
-			//RequestDispatcher rd = req.getRequestDispatcher("attemptLogin");
-			//rd.forward(req, resp);
+		if (value.equals("Login")) {	
+			String result = "";
+			
+			if (LoginDao.validate(name, password)) result = "Successful, logging in";
+			else result = "Incorrect email or password provided, try again.";
+			
+			req.setAttribute("message", result);
+		} else if (value.equals("Post")) {
+			final String status = (String)req.getParameter("statusText");
+			
+			if (status != null) {
+				String[] update = NewsFeedDAO.newTextPost(status);
+				if (updates == null) updates = update;
+				else {
+					updates = expandArray(updates, update);
+				}
+				req.setAttribute("updates", updates);
+			}
 		}
-		else {
-			out.println("Sorry username or password incorrect.");
-			//RequestDispatcher rd = req.getRequestDispatcher("index.html");
-			//rd.include(req, resp);
-		}
-		out.close();
+		req.getRequestDispatcher("Home.jsp").forward(req, resp);
+	}
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher("Home.jsp").forward(req, resp);
+	}
+	
+	public String[] update(HttpServletRequest req, HttpServletResponse resp, String passedInformation) throws ServletException, IOException {
+		
+		return NewsFeedDAO.newTextPost(passedInformation);
+	}
+
+	private synchronized Serializable createUID() {
+		UUID uniqueKey = UUID.randomUUID();
+		return uniqueKey.toString();
 	}
 }
--- Social-Network/src/com/distributed/networksocial/handlers/LoginDao.java
+++ Social-Network/src/com/distributed/networksocial/handlers/LoginDao.java
@@ -1,5 +1,17 @@
 package com.distributed.networksocial.handlers;
 
+import java.util.Collections;
+
+import org.datanucleus.util.Base64;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+
 /**
  * Login Validation template, this class will oversee all Login attempts, errors and successful attempts.
  * Possibly handle validation of new accounts, password resets and lockouts for accounts?
@@ -8,6 +20,8 @@ package com.distributed.networksocial.handlers;
  */
 public class LoginDao {
 
+	protected static Entity currentRequestedUser = null;
+	
 	/**
 	 * This class will detect if the supplied username and password are a valid combination,
 	 *	after this we can call our new method {@code} retriveUniqueID. Which will be used for
@@ -20,12 +34,39 @@ public class LoginDao {
 		boolean status = false;
 		
 		try {
-		/**
-		 * Check the datastore for the username and passsword, do they matcH?
-		 * If so return true, else return false.
-		 */
+			final DatastoreService store = DatastoreServiceFactory.getDatastoreService();
+			Entity user = null;
+
+			if (entityExists(name)) {
+				user = currentRequestedUser;
+				currentRequestedUser = null;
+				
+				System.out.println(Base64.decodeString((String) user.getProperty("password")));
+				
+				if (((String)user.getProperty("password")).equals(encryptPassword(pass))) {
+					status = true;
+				}
+			}
+				
 		} catch (Exception e) {
-		}
+			System.out.println(e.getMessage());
+		}	
 		return status;
+	}
+	
+	protected static boolean entityExists(String name) {
+		final Query q = new Query("Users");
+		final PreparedQuery pq = DatastoreServiceFactory.getDatastoreService().prepare(q);
+		for (final Entity e : pq.asIterable()) {
+			if (((String)e.getProperty("email")).equals(name)) {
+				currentRequestedUser = e;
+				return true; 
+			}
+		}
+		return false;
+	}
+	
+	protected static String encryptPassword(String pass) {
+		return Base64.encodeString(pass);
 	}
 }
