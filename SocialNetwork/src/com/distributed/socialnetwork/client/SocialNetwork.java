e78728202f06cf75b6c377874101b06900795ecc
Christopher Hayes <Christopher Hayes@DESKTOP-FVQDKPD>
2016-11-24 15:48:26 +0000

    Fixed WEB.xml
    ...

--- SocialNetwork/src/com/distributed/socialnetwork/client/SocialNetwork.java
+++ SocialNetwork/src/com/distributed/socialnetwork/client/SocialNetwork.java
@@ -33,7 +33,7 @@ public class SocialNetwork implements EntryPoint {
 	/**
 	 * Create a remote service proxy to talk to the server-side Greeting service.
 	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+	private final GreetingServiceAsync greetingService = GWT.create(ConnectionService.class);
 
 	/**
 	 * This is the entry point method.
