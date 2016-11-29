0d842a3e9d76095013288f8c1e5f44d8c3a2afef
Alex Brightmore <alexbrightmore@hotmail.co.uk>
2016-11-29 18:25:58 +0000

    Adding photogallery widget to GalleryView

--- SocialNetwork/src/com/distributed/socialnetwork/client/gallery/GalleryView.java
+++ SocialNetwork/src/com/distributed/socialnetwork/client/gallery/GalleryView.java
@@ -9,41 +9,36 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class GalleryView extends Composite {
 
-	VerticalPanel vPanel = new VerticalPanel();
+	private VerticalPanel vPanel = new VerticalPanel();
+	
+	private ContentGallery galleryWidget;
+	private UploadContent uploadWidget;
 	
 	public GalleryView() {
 		int windowHeight = Window.getClientHeight();
 		int windowWidth = Window.getClientWidth();
 		
-	  DockPanel dockPanel = new DockPanel();
-	  dockPanel.setStyleName("dockpanel");
-	  dockPanel.setSpacing(4);
-	  dockPanel.setHorizontalAlignment(DockPanel.ALIGN_CENTER);
+	    DockPanel dockPanel = new DockPanel();
+	    dockPanel.setStyleName("dockpanel");
+	    dockPanel.setSpacing(4);
+	    dockPanel.setHorizontalAlignment(DockPanel.ALIGN_CENTER);
 	
-	  // Add text all around
-	  dockPanel.add(new HTML("This is the first north component."), 
-	  DockPanel.NORTH);
-	  dockPanel.add(new HTML("This is the first south component."), 
-	  DockPanel.SOUTH);
-	  dockPanel.add(new HTML("This is the west component."), 
-	  DockPanel.WEST);
+	    // Add text all around
+	    dockPanel.add(new HTML("This is the first north component."), 
+	    DockPanel.NORTH);
+	    dockPanel.add(new HTML("This is the first south component."), 
+	    DockPanel.SOUTH);
+	    dockPanel.add(new HTML("This is the west component."), 
+	    DockPanel.WEST);
 	
-	  // Add scrollable text in the center
-	  HTML contents = new HTML("This is a ScrollPanel contained"
-	  +" at the center of a DockPanel. "
-	  +" By putting some fairly large contents in the middle"
-	  +" and setting its size explicitly, it becomes a scrollable area"
-	  +" within the page, but without requiring the use of an IFRAME."
-	  +" Here's quite a bit more meaningless text that will serve primarily"
-	  +" to make this thing scroll off the bottom of its visible area."
-	  +" Otherwise, you might have to make it really, really"
-	  +" small in order to see the nifty scroll bars!");
-	  ScrollPanel scroller = new ScrollPanel(contents);
-	  scroller.setPixelSize(windowHeight, windowWidth);
-	  dockPanel.add(scroller, DockPanel.CENTER);
+	    uploadWidget.addGalleryUpdatedEventHandler(galleryWidget);
+	    // Add scrollable text in the center
+	    ScrollPanel scroller = new ScrollPanel(uploadWidget);
+	    scroller.setPixelSize(windowHeight, windowWidth);
+	    dockPanel.add(scroller, DockPanel.CENTER);
 	
-	  // Add the widgets to the root panel.
-      vPanel.add(dockPanel);
+	    // Add the widgets to the root panel.
+        vPanel.add(dockPanel);
 	}
 
 	public VerticalPanel getMainPanel() {
